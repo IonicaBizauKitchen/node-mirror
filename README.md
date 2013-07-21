d82d49ad6b3703238236b6d05d331138ae9c7342
zeke <zeke@sikelianos.com>
2013-07-21 01:44:27 -0700

    it works, more or less

---
+++ README.md
@@ -0,0 +1,14 @@
+# Node Mirror
+
+Copy compiled versions of Node.js from nodejs.org to S3.
+
+## How it works
+
+1. Parse Nodejs.org's [list of versions](http://nodejs.org/dist/npm-versions.txt)
+2. Find all versions already copied to S3.
+3. Diff the two lists and copy any new versions (above 0.8.6) from nodejs.org to S3.
+
+## Example URLs
+
+- http://nodejs.org/dist/v0.10.13/node-v0.10.13-linux-x64.tar.gz
+- http://heroku-node-mirror.s3.amazonaws.com/node-v0.10.13-linux-x64.tar.gz
--- app.js
+++ app.js
@@ -1,35 +1,51 @@
-var request = require("superagent");
 var _ = require("lodash");
+var http = require("http");
+var agent = require("superagent");
+var semver = require("semver");
+var knox = require("knox");
 
-var client = knox.createClient({
-  key: '<api-key-here>',
-  secret: '<secret-here>',
-  bucket: 'learnboost'
+var s3 = knox.createClient({
+  key: process.env.AWS_KEY,
+  secret: process.env.AWS_SECRET,
+  bucket: process.env.S3_BUCKET
 });
 
-http.get("http://nodejs.org/dist/npm-versions.txt", function(res){
+function copyToS3(url) {
+  console.log(url);
+
+  http.get(url, function(res){
+    var headers = {
+      'Content-Length': res.headers['content-length'],
+      'Content-Type': res.headers['content-type']
+    };
+    var filename = res.req.path.replace(/^.*[\\\/]/, '');
+    console.log(filename);
+    s3.putStream(res, "/"+filename, headers, function(err, res){
+      if (err) console.error(err);
+      console.log(res);
+      console.log("logic");
+    });
+  });
+
+}
+
+agent.get("http://nodejs.org/dist/npm-versions.txt", function(res){
 
   var lines = res.text.split("\n");
 
   var urls = lines.map(function(line) {
-    var nodeVersion = line.match("(v.*) ")
-    if (!nodeVersion) return;
-    return "http://nodejs.org/dist/" + nodeVersion[1] + "/node-" + nodeVersion[1] + "-linux-x64.tar.gz";
+    var match = line.match("v(.*) ")
+    if (!match) return;
+    var version = match[1];
+
+    // 0.8.6 is the first available version on nodejs.org
+    if (semver.lt(version, '0.8.6')) return;
+
+    return "http://nodejs.org/dist/v" + version + "/node-v" + version + "-linux-x64.tar.gz";
   })
 
   urls = _.compact(urls);
 
-  console.log(urls);
-
-  http.get('http://google.com/doodle.png', function(res){
-    var headers = {
-        'Content-Length': res.headers['content-length']
-      , 'Content-Type': res.headers['content-type']
-    };
-    client.putStream(res, '/doodle.png', headers, function(err, res){
-      // Logic
-    });
-  });
-
+  _.forEach(urls, copyToS3)
 
 });
